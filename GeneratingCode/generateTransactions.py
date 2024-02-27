8ec49864bdb200195468158938bd5aebc9ef3204
Matt <65925728+MatthewAdragna@users.noreply.github.com>
2024-02-27 16:09:33 -0500

    Created the generator for part 2 aswell

---
+++ GeneratingCode/generateTransactions.py
@@ -0,0 +1,64 @@
+import argparse
+import random
+import string
+
+MBLINES = (int)(100000 / 1.7)
+
+customers = 0
+
+
+# Assuming that each line is approximately 10 bytes long
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--customers",
+        help="Amount of customers generated",
+        type=int,
+        default=50000,
+    )
+    parser.add_argument(
+        "--purchases",
+        help="Amount of purchases created",
+        type=int,
+        default=5000000,
+    )
+    parser.add_argument(
+        "--path",
+        help="Path where the files will be created",
+        type=str,
+        default="../input/",
+    )
+    args = parser.parse_args()
+    customers = args.customers
+    with open(args.path + "customers.csv", "w") as custFile:
+        for i in range(0, args.customers):
+            custFile.write(generateCustomer(i))
+
+    with open(args.path + "purchases.csv", "w") as purchFile:
+        for i in range(0, args.purchases):
+            purchFile.write(generatePurchase(i))
+
+
+def generateCustomer(id):
+    return "{},{},{},{},{}\n".format(
+        id,
+        "".join(random.choices(string.ascii_lowercase, k=5)),
+        random.randint(18, 100),
+        random.randint(1, 500),
+        random.uniform(100, 10000000),
+    )
+
+
+def generatePurchase(id):
+    return "{},{},{},{},{}\n".format(
+        id,
+        random.randint(0, customers),
+        random.uniform(10, 2000),
+        random.randint(1, 15),
+        "".join(random.choices(string.ascii_lowercase, k=random.randint(20, 50))),
+    )
+
+
+if __name__ == "__main__":
+    main()
+
--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 IN ORDER TO GENERATE DATA:
-  CD to the GeneratingCode folder
-  Run the generateFiles code by using
-    py generateFiles.py 
-    with optional arguments 
-      --path "PUT YOUR PATH IN HERE" (default is ../input/)
-      --covidchance inputChance (default is .01)
-      --size inputSizeInMB (default is 10)
+  ```
+    CD to the GeneratingCode folder:
+    FOR CONCERT DATA:
+          Run the generateConcert code by using
+            py generateConcert.py
+            with optional arguments
+              --path "PUT YOUR PATH IN HERE" (default is ../input/)
+              --covidchance inputChance (default is .01)
+              --size inputSizeInMB (default is 10)
+           
   
-
+  
+    FOR TRANSACTIONDATA:
+      Run the generateTransactions code by using
+        py generateTransactions.py
+          with optional arguments
+            --path "PUT YOUR PATH IN HERE" (default is ../input/)
+            --customers int (default is 50000)
+            --purchases int (default is 5000000)
+            ```
